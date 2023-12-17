c4882064f7c76099513627b0bc53b5fe0b85582b
Jurfest <jurfest.apps@gmail.com>
2023-12-17 02:14:30 -0300

    fix: adjust IP extraction from json output

--- ansible/create_hosts.sh
+++ ansible/create_hosts.sh
@@ -3,8 +3,13 @@
 # Directory where Terraform files are located
 TERRAFORM_DIR="../terraform"
 
-# Navigate to the Terraform directory and get the output
-IP=$(terraform -chdir=$TERRAFORM_DIR output --raw public_ips)
+# Navigate to the Terraform directory and get the output in JSON format
+OUTPUT_JSON=$(terraform -chdir=$TERRAFORM_DIR output --json)
+
+echo "OUTPUT_JSON: $OUTPUT_JSON"
+
+# Extract the IP address from the JSON output
+IP=$(echo "$OUTPUT_JSON" | jq -r '.public_ips.value[0]')  # Assumes public_ips is a list, adjust if needed
 
 # Check if the IP was obtained
 if [ -z "$IP" ]; then
@@ -16,16 +21,16 @@ fi
 ANSIBLE_DIR="."
 
 # Directory where SSH keys are located
-SSH_KEYS_DIR="ssh-keys"
+SSH_KEYS_DIR="../ssh-keys"
 
 # Create the inventory file
-cat << EOF > "$ANSIBLE_DIR/hosts.ini"
+cat << EOF > "$ANSIBLE_DIR/inventory.ini"
 [webservers]
 $IP
 
 [webservers:vars]
-ansible_ssh_user=ubuntu
+ansible_ssh_user=azureadmin
 ansible_ssh_private_key_file="$SSH_KEYS_DIR/swarm-cluster"
 EOF
 
-echo "Inventory file created at $ANSIBLE_DIR/hosts.ini"
+echo "Inventory file created at $ANSIBLE_DIR/inventory.ini"
